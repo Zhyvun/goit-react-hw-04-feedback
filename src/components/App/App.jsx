040f94eb3ef9aa8d8c2d7b98e910596d1e189c93
Zhyvun Volodymyr <zhionik@gmail.com>
2023-06-27 23:39:36 +0300

    Create HW

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,59 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import React from 'react';
+import { useState } from 'react';
 import { Container } from './App.styled';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../Statistics/Statistics';
 import { Section } from '../Section/Section';
 import { Notification } from '../Notification/Notification';
 
+//* HOOKS
+export const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = option => {
+    setFeedback(prevState => ({
+      ...prevState,
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  const totalFeedback = Object.values(feedback).reduce(
+    (total, currentValue) => total + currentValue
+  );
+
+  const positivePercentage = Math.round((feedback.good / totalFeedback) * 100);
+
+  return (
+    <Container>
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        onLeaveFeedback={handleFeedback}
+        options={Object.keys(feedback)}
+      />
+    </Section>
+    <Section title="Statistics">
+      {totalFeedback ? (
+        <Statistics
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <Notification alert="There is no feedback" />
+      )}
+    </Section>
+  </Container>
+  );
+};
+
+/* CLASS !
 export class App extends Component {
   state = {
     good: 0,
@@ -60,3 +109,4 @@ export class App extends Component {
     );
   }
 }
+*/
